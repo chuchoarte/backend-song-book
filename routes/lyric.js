e47aeef930e2c9786f81332b90b3954f489a218c
Jesus <surjesus.sistemas@gmail.com>
2021-07-31 00:20:48 -0500

    added mao function

--- routes/lyric.js
+++ routes/lyric.js
@@ -12,20 +12,29 @@ router.get('/artist/:artist/title/:title', async (req, res) => {
     const response = await axios.get('https://api.genius.com/search?access_token='+API_KEY+'&q='+q);
     const data = response.data.response.hits[0];
 
-    if (Similarity(data.result.full_title, q) > 0.55) {
-        const url = 'https://api.genius.com/songs/';
-        const headers = {
-            'Authorization': 'Bearer ' + API_KEY
-        };
-        let {
-            data: { response: { song } }
-        } = await axios.get(url + data.result.id, { headers });
-        req.params.lyrics = await ExtractLyrics(song.url);
-    }
+    const getLyrics = await Promise.all(response.data.response.hits.map(async item => {
+        if (Similarity(item.result.full_title, q) > 0.55) {
+            const url = 'https://api.genius.com/songs/';
+            const headers = {
+                'Authorization': 'Bearer ' + API_KEY
+            };
+            let {
+                data: { response: { song } }
+            } = await axios.get(url + item.result.id, { headers });
+            return await ExtractLyrics(song.url);
+        }
+    }));
+
+    let getLyricFiltered = getLyrics.filter(item => {
+        return item !== undefined;
+    });
+
+    const lyrics = getLyricFiltered[0];
+
     res.json({
         ok: true,
         'full_title': data.result.full_title,
-        'lyrics': req.params.lyrics,
+        'lyrics': lyrics,
     });
 });
 
